7485e130397a9dd6618f87ec94d20af6aa615f3d
Alikhan <astanaproteam123@gmail.com>
2023-04-30 17:51:26 +0600

    adding documentation to MyLinkedListStack

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,13 +1,23 @@
+/**
+
+ A stack implementation using a linked list.
+ This class uses the built-in LinkedList class provided by Java.
+ @param <E> the type of elements in the stack.
+ */
 package src;
 import java.util.EmptyStackException;
 import java.util.LinkedList;
 public class MyLinkedListStack <E> {
     private LinkedList<E> list;
 
+    /**
+     * Constructs an empty stack.
+     */
     public MyLinkedListStack() {
         list = new LinkedList<>();
     }
 
+    
     public void push(E element) {
         list.addFirst(element);
     }
