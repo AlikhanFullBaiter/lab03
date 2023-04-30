661a7e9ecd411bdd34fb3d8e6d536cd4ef93ddf0
Alikhan <astanaproteam123@gmail.com>
2023-04-30 15:41:04 +0600

    adding constructor

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,6 +1,10 @@
 
 package src;
-import java.util.LinkedList
+import java.util.LinkedList;
 public class MyLinkedListQueue <E> {
     private LinkedList<E> list;
+    public MyLinkedListQueue(){
+        list = new LinkedList<>();
+    }
+
 }
