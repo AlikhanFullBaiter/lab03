784552f172e2cf2b83ae08396b8f7712cea4a5cb
Alikhan <astanaproteam123@gmail.com>
2023-04-30 15:17:11 +0600

    constructor

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -3,4 +3,8 @@ import java.util.EmptyStackException;
 import java.util.LinkedList;
 public class MyLinkedListStack <E> {
     private LinkedList<E> list;
+
+    public MyLinkedListStack() {
+        list = new LinkedList<>();
+    }
 }
