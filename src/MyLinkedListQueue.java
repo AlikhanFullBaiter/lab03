7c22f1c0832730bbf6c29e3ab8e215f50f578ce2
Alikhan <astanaproteam123@gmail.com>
2023-04-30 15:43:39 +0600

    adding enqueue(add) and dequeue(remove) methods

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,10 +1,22 @@
 
 package src;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 public class MyLinkedListQueue <E> {
     private LinkedList<E> list;
     public MyLinkedListQueue(){
         list = new LinkedList<>();
     }
 
+    public void enqueue(E element) {
+        list.addFirst(element);
+    }
+
+    public E dequeue() {
+        if (list.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.removeFirst();
+    }
 }
